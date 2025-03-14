addf00b75485c6f9e3f6c71d5c61932ce8cd732b
Jake Bills <jakebills1@gmail.com>
2025-03-14 10:08:49 -0600

    breaks out IO into module

--- lib/redis/server.rb
+++ lib/redis/server.rb
@@ -2,10 +2,12 @@ require "socket"
 require "nio4r"
 require_relative './logger'
 require_relative './command'
+require_relative './protocol_io'
 
 module Redis
   class Server
     include Logger
+    include ProtocolIO
 
     def initialize(port)
       @port = port
@@ -87,24 +89,6 @@ module Redis
       selector.close
       # log 'redis shutting down gracefully'
       exit
-    end
-
-    def read_bulk_string(io)
-      len = read_token(io).delete("$").to_i
-      # + 2 bytes for the separator
-      read_n_bytes(io, len + 2)
-    end
-
-    def read_token(io)
-      buf = ""
-      io.read_nonblock(4, buf)
-      buf.delete("\r\n")
-    end
-
-    def read_n_bytes(io, n_bytes)
-      buf = ""
-      io.read_nonblock(n_bytes, buf)
-      buf.delete("\r\n")
     end
   end
 end
